308683d57335073e5e88d9554e3d4cede7eca875
2dowon <dwon424@gmail.com>
2021-08-26 15:13:18 +0900

    ✨ [feat] show web-view

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+import {WebView} from 'react-native-webview';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello, world!</Text>
+    <View style={styles.container}>
+      <WebView
+        style={styles.webview}
+        source={{uri: 'https://www.goods-duck.com/'}}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+  },
+  webview: {
+    flex: 1,
+  },
+});
 
 export default App;
